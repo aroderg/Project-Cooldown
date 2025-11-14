597e366b6e17822908f47ff5ca062dbcf93ed5e6
aroderg <anothersoul1staken@gmail.com>
2025-11-14 23:20:45 +0200

    v25-11-14

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(_)
-    _.window.title = "Project: Cooldown, v25-08-19"
+    _.window.title = "Project: Cooldown, v25-11-14"
     _.window.width = 1280
     _.window.height = 720
     _.console = true
